cb8de3fec0a5ad372ad49b04ee64bed8bbf475cf
Radosław Palczyński <radoslaw.palczynski@nordcloud.com>
2019-04-25 14:23:24 +0200

    Refactor operation loging install job

--- vars/terraformPlan.groovy
+++ vars/terraformPlan.groovy
@@ -2,16 +2,23 @@
 
 def call(Map config) {
     with_aws = false
+    extraArgs = ""
     if (config.containsKey('withAWS')) {
         with_aws = config.withAWS
     }
+
+    if (config.containsKey('extraArgs')) {
+        extraArgs = config.extraArgs
+    }
+
+
 
     if (with_aws) {
         dir("${config.dir}") {
             ansiColor('xterm') {
                 withProxyEnv() {
                     withAWS(role: "$CROSS_ACCOUNT_ROLE_NAME", roleAccount: "$AWS_APPLICATION_ACCOUNT_NUMBER") {
-                        sh "/usr/bin/terraform plan -var-file=\"../terraform.tfvars\" -out=tfplan -input=false ${config.extraArgs}"
+                        sh "/usr/bin/terraform plan -var-file=\"../terraform.tfvars\" -out=tfplan -input=false ${extraArgs}"
                     }
                 }
             }
@@ -20,7 +27,7 @@ def call(Map config) {
         dir("${config.dir}") {
             ansiColor('xterm') {
                 withProxyEnv() {
-                    sh "/usr/bin/terraform plan -var-file=\"../terraform.tfvars\" -out=tfplan -input=false ${config.extraArgs}"
+                    sh "/usr/bin/terraform plan -var-file=\"../terraform.tfvars\" -out=tfplan -input=false ${extraArgs}"
                 }
             }
         }
